8a9d92693fa53960a76f795f51dcde8bc1a9ec44
Samuel Goodwin <samuel.goodwin@me.com>
2012-03-14 01:22:33 -0400

    Initial Commit

---
+++ CLIProject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CLIProject
+//
+//  Created by Samuel Goodwin on 3/14/12.
+//  Copyright (c) 2012 SNAP Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
